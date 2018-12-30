2d75f875dc51f55d31a30f4cad4acb165f3a9d6a
Bin Shen <binshen@hioscar.com>
2018-12-30 13:53:10 -0500

    cleanup App.js

--- js/App.js
+++ js/App.js
@@ -1,10 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+// @flow
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
