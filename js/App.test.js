0cf5b69d440e35b00603d70d4ca5d9657390d70d
Bin Shen <binshen@hioscar.com>
2018-12-30 14:08:11 -0500

    setup jest test

---
+++ js/App.test.js
@@ -0,0 +1,13 @@
+// @flow
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import App from './App';
+
+
+describe('App', () => {
+  it('renders', () => {
+    const component = shallow(<App />);
+    expect(component).toMatchSnapshot();
+  });
+});
---
+++ js/test/setupAfterEnv.js
@@ -0,0 +1,6 @@
+import {configure} from 'enzyme';
+import {createSerializer} from 'enzyme-to-json';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({}));
